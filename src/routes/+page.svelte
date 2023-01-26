6b334f21a8e1e12e4e5b7d794bbdf3a2528e42fd
Martz <github@jmartz.de>
2023-01-26 12:25:55 +0100

    start with homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,4 @@
-<h2>Willkommen auf der Seite  :D</h2>
+<div class="grid grid-cols-6">
+    <div class="col-span-6 md:col-span-2">Test</div>
+    <div class="col-span-6 md:col-span-4">Test</div>
+</div>
